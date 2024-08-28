c1e4af3f1d52481d93204255a688e00e810c9cc3
lizbethMelissaPS <melissasud27@gmail.com>
2024-08-28 04:46:51 -0500

    logueo con usuario valido ejecuto

--- src/test/java/com/nttdata/steps/LoginSteps.java
+++ src/test/java/com/nttdata/steps/LoginSteps.java
@@ -2,6 +2,7 @@ package com.nttdata.steps;
 
 import com.nttdata.page.LoginPage;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -9,9 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-
-import static com.nttdata.page.LoginPage.popupMessage;
-import static com.nttdata.page.LoginPage.totalAmount;
+import static com.nttdata.page.LoginPage.*;
 
 
 public class LoginSteps {
@@ -30,11 +29,23 @@ public class LoginSteps {
      * @param user el usuario
      */
     public void typeUser(String user){
-        WebElement userInputElement = driver.findElement(LoginPage.userInput);
-        userInputElement.sendKeys(user);
+        // Encuentra el campo de correo electrónico utilizando el ID
+        WebElement emailInput = driver.findElement(By.id("field-email"));
+
+        // Imprime el valor para depuración
+        System.out.println("Campo de email encontrado: " + emailInput);
+
+        // Enviar texto al campo de correo electrónico
+        emailInput.sendKeys(user);
+
+        // Imprime el valor ingresado para verificación
+        String valorIngresado = emailInput.getAttribute("value");
+        System.out.println("Valor ingresado en el campo de email: " + valorIngresado);
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(444));
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(LoginPage.loginButton));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("submit-login")));
+
 
 
     }
@@ -44,14 +55,34 @@ public class LoginSteps {
      * @param password el password del usuario
      */
     public void typePassword(String password){
-        this.driver.findElement(LoginPage.passInput).sendKeys(password);
+        // Encuentra el campo de contraseña utilizando el ID
+        WebElement passwordInput = driver.findElement(By.id("field-password"));
+
+        // Imprime el valor para depuración
+        System.out.println("Campo de contraseña encontrado: " + passwordInput);
+
+        // Enviar texto al campo de contraseña
+        passwordInput.sendKeys(password);
+
+        // Imprime el valor ingresado para verificación
+        String valorIngresado = passwordInput.getAttribute("value");
+        System.out.println("Valor ingresado en el campo de contraseña: " + valorIngresado);
+
     }
 
     /**
      * Hacer click en el botón login
      */
+
     public void login(){
-        this.driver.findElement(LoginPage.loginButton).click();
+        // Encuentra el botón de inicio de sesión utilizando el ID
+        WebElement loginButton = driver.findElement(By.id("submit-login"));
+
+        // Imprime el valor para depuración
+        System.out.println("Botón de inicio de sesión encontrado: " + loginButton);
+
+        // Hacer clic en el botón de inicio de sesión
+        loginButton.click();
     }
 
     public void typeCategoria(String categoria) {
